a38c9b4d7a99fc8447748bbea658f70204e3ad32
Musa Arslan <officialmusa58@gmail.com>
2024-05-24 13:17:50 +0200

    last updates

--- components/pages/Homepage.js
+++ components/pages/Homepage.js
@@ -7,7 +7,7 @@ function Homepage(){
             <div className={classes.title}>Welcome to Freshfamilyguide</div>
             <div className={classes.description}>Dive into a World of Engaging Articles on Raising a Child, explore our extensive collection of insightful and inspiring articles, offering practical tips on every aspect of parenting. Join our community and discover new ideas to support your child{"'"}s growth and development.</div>
             <div className={classes.buttons}>
-                <Link href={"/articles"}><div className={classes.button}>Discover</div></Link>
+                <Link href={"/articles/nutrition"}><div className={classes.button}>Discover</div></Link>
                 <Link href={"/signup"}><div className={classes.button}>Get Started</div></Link>
             </div>
             <div className={classes.background}>
